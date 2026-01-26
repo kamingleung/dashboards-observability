25847c5400d4a797973958452ab7269b1cf5e1d6
kamingleung <kamingleung@users.noreply.github.com>
2026-01-26 13:17:44 -0800

    APM empty state panel style adjustments.
    Signed-off-by: kamingleung <kamingleung@users.noreply.github.com>

--- public/components/apm/common/apm_empty_state.tsx
+++ public/components/apm/common/apm_empty_state.tsx
@@ -6,7 +6,7 @@
 import React, { useState } from 'react';
 import {
   EuiButton,
-  EuiLink,
+  EuiButtonEmpty,
   EuiSpacer,
   EuiText,
   EuiButtonGroup,
@@ -16,7 +16,10 @@ import {
   EuiPanel,
 } from '@elastic/eui';
 import servicesPreview from './assets/services-preview.png';
+import mapPreview from './assets/maps-preview.png';
+import tracesPreview from './assets/traces-preview.png';
 import { apmEmptyStateI18nTexts as i18nTexts, getPreviewImageAlt } from './apm_empty_state_i18n';
+import './apm_empty_state.scss';
 
 const APM_DOCS_URL = 'https://docs.opensearch.org/latest/observing-your-data/';
 
@@ -24,6 +27,7 @@ interface TabContent {
   id: string;
   name: string;
   description: string;
+  previewImage: string;
 }
 
 const options: TabContent[] = [
@@ -31,16 +35,19 @@ const options: TabContent[] = [
     id: 'services',
     name: i18nTexts.tabs.services.name,
     description: i18nTexts.tabs.services.description,
+    previewImage: servicesPreview,
   },
   {
     id: 'application-map',
     name: i18nTexts.tabs.applicationMap.name,
     description: i18nTexts.tabs.applicationMap.description,
+    previewImage: mapPreview, // TODO: Replace with application-map-preview.png when available
   },
   {
     id: 'correlate-traces-logs',
     name: i18nTexts.tabs.correlateTracesLogs.name,
     description: i18nTexts.tabs.correlateTracesLogs.description,
+    previewImage: tracesPreview, // TODO: Replace with correlate-traces-logs-preview.png when available
   },
 ];
 
@@ -61,35 +68,40 @@ export const ApmEmptyState = ({ onGetStartedClick }: ApmEmptyStateProps) => {
   return (
     <EuiFlexGroup direction="column" alignItems="center" gutterSize="none">
       <EuiFlexItem grow={false}>
-        <EuiSpacer size="xxl" />
-        <EuiText textAlign="center">
-          <h1>{i18nTexts.title}</h1>
-        </EuiText>
+        {/* Panel containing all content */}
+        <EuiPanel paddingSize="l" className="apmEmptyStatePanel">
+          <EuiText textAlign="center">
+            <h1>{i18nTexts.title}</h1>
+          </EuiText>
 
-        <EuiSpacer size="l" />
+          <EuiSpacer size="l" />
 
-        <EuiFlexGroup justifyContent="center" gutterSize="m">
-          <EuiFlexItem grow={false}>
-            <EuiButton fill onClick={onGetStartedClick}>
-              {i18nTexts.getStarted}
-            </EuiButton>
-          </EuiFlexItem>
-        </EuiFlexGroup>
+          <EuiFlexGroup justifyContent="center" gutterSize="m">
+            <EuiFlexItem grow={false}>
+              <EuiButton fill onClick={onGetStartedClick}>
+                {i18nTexts.getStarted}
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
 
-        <EuiSpacer size="l" />
+          <EuiSpacer size="s" />
 
-        <EuiFlexGroup justifyContent="center">
-          <EuiFlexItem grow={false}>
-            <EuiLink href={APM_DOCS_URL} target="_blank" external={false}>
-              {i18nTexts.viewDocs}
-            </EuiLink>
-          </EuiFlexItem>
-        </EuiFlexGroup>
+          <EuiFlexGroup justifyContent="center">
+            <EuiFlexItem grow={false}>
+              <EuiButtonEmpty
+                href={APM_DOCS_URL}
+                target="_blank"
+                iconType="popout"
+                iconSide="right"
+                color="primary"
+              >
+                {i18nTexts.viewDocs}
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+          </EuiFlexGroup>
 
-        <EuiSpacer size="xxl" />
+          <EuiSpacer size="xl" />
 
-        {/* Panel containing button group, description, and preview */}
-        <EuiPanel paddingSize="l">
           {/* Button Group */}
           <EuiFlexGroup justifyContent="center">
             <EuiFlexItem grow={false}>
@@ -118,7 +130,7 @@ export const ApmEmptyState = ({ onGetStartedClick }: ApmEmptyStateProps) => {
             style={{
               border: '2px solid transparent',
               borderRadius: '6px',
-              overflow: 'hidden',
+              // overflow: 'hidden',
               maxWidth: '900px',
               margin: '0 auto',
               backgroundOrigin: 'padding-box, border-box',
@@ -126,7 +138,8 @@ export const ApmEmptyState = ({ onGetStartedClick }: ApmEmptyStateProps) => {
             }}
           >
             <EuiImage
-              src={servicesPreview}
+              hasShadow={true}
+              src={selectedTab.previewImage}
               alt={getPreviewImageAlt(selectedTab.name)}
               style={{ width: '100%', display: 'block' }}
             />
